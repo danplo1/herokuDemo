7a10c687857ba2dfc273404185f02f623221f704
danplo1 <budweiser401@wp.pl>
2019-02-01 22:53:19 +0100

    another one file

--- src/main/java/com/clockworkjava/herokuDemo/HelloController.java
+++ src/main/java/com/clockworkjava/herokuDemo/HelloController.java
@@ -1,6 +1,8 @@
 package com.clockworkjava.herokuDemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+    PeopleRepository peopleRepository;
+
+    @Autowired
+    public HelloController(PeopleRepository peopleRepository) {
+
+        this.peopleRepository = peopleRepository;
+    }
+
     @RequestMapping("hi")
-    public String hello (){
+    public String hello(Model model) {
+        model.addAttribute("people", peopleRepository.findAll());
         return "hello";
 
     }
